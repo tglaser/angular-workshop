7ac65ffbd4e646e9e676e5e4364f87f8895900d4
Alex Lechner <alechner@it-economics.de>
2018-12-10 16:20:20 +0100

    Input, Kontrollstrukturen ngIf, ngFor

--- src/app/components/hello/hello.component.ts
+++ src/app/components/hello/hello.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-hello',
@@ -7,12 +7,22 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HelloComponent implements OnInit {
 
-  name: string = 'Alex';
+  @Input()
+  name: string;
+
+  names: string[] = ['Benjamin', 'Alex', 'Sigrid'];
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  get displayName(): string {
+    if (this.name) {
+      return this.name;
+    }
+    return 'World';
   }
 
 }
