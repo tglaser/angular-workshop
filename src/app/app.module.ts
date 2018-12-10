3023d9e3b1ada2049b6dce58a11984341743f883
Alex Lechner <alechner@it-economics.de>
2018-12-10 16:36:39 +0100

    click binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './components/hello/hello.component';
+import { Hello2Component } from './components/hello2/hello2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
+    HelloComponent,
+    Hello2Component
   ],
   imports: [
     BrowserModule,
